b9e35a19b8fc8e90016f6e4285f36b2dd8066cd7
bhullarravia <bhullarravia@gmail.com>
2024-06-21 09:45:37 -0400

    Initial Commit

---
+++ About Me/About_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_MeApp.swift
+//  About Me
+//
+//  Created by Ravia Bhullar on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct About_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
